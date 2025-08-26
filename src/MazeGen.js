8051de5968bfb53e237669a6772e62968e1f973a
Tiramisu <71141838+AlexSchoolOH@users.noreply.github.com>
2025-08-26 14:16:20 -0400

    Add Tiles

--- src/MazeGen.js
+++ src/MazeGen.js
@@ -1,10 +1,23 @@
 HazyMaze.generate = () => {
-    const mazyPosition = new Int8Array(2);
+    const mazyPosition = new Uint16Array(2);
 
     mazyPosition[0] = Math.randomIRange(0, HazyMaze.level.width - 1);
     mazyPosition[1] = Math.randomIRange(0, HazyMaze.level.height - 1);
 
     HazyMaze.level.setTile(...mazyPosition, HazyMaze.level.getTile(...mazyPosition) ^ HazyMaze.WEST);
 
-    return mazyPosition;
+    const worm = () => {
+        const oldPos = new Uint16Array(mazyPosition);
+        console.log(`Generating from ${oldPos}`);
+
+        console.log(HazyMaze.level.getPathsFrom(...mazyPosition));
+
+        //Go back to old position;
+        mazyPosition[0] = oldPos[0];
+        mazyPosition[1] = oldPos[1];
+    }
+
+    worm()
+
+    return HazyMaze.level.tiles;
 };
--- src/level.js
+++ src/level.js
@@ -15,11 +15,30 @@ HazyMaze.level = {
     },
     //Tile modification
     setTile: (x, y, val) => {
+        //Make sure we aren't OOB
+        if (x < 0 || x >= HazyMaze.level.width) return 0;
+        if (y < 0 || y >= HazyMaze.level.height) return 0;
+
         HazyMaze.level.tiles[(y * HazyMaze.level.width) + x] = val;
         return HazyMaze.level.getTile(x, y);
     },
     getTile: (x, y) => {
+        //Make sure we aren't OOB
+        if (x < 0 || x >= HazyMaze.level.width) return 0;
+        if (y < 0 || y >= HazyMaze.level.height) return 0;
+
         return HazyMaze.level.tiles[(y * HazyMaze.level.width) + x];
+    },
+    getPathsFrom: (x, y) => {
+        let returned = 0;
+
+        //Check directions and augment accordingly
+        if (HazyMaze.level.getTile(x+1, y) & HazyMaze.WEST) returned |= HazyMaze.EAST;
+        if (HazyMaze.level.getTile(x-1, y) & HazyMaze.EAST) returned |= HazyMaze.WEST;
+        if (HazyMaze.level.getTile(x, y+1) & HazyMaze.SOUTH) returned |= HazyMaze.NORTH;
+        if (HazyMaze.level.getTile(x, y-1) & HazyMaze.NORTH) returned |= HazyMaze.SOUTH;
+
+        return returned;
     },
     //Level data
     width:1,
