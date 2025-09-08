38d2cde3301238b73e505285419d3c1b25ec3051
Tiramisu <71141838+AlexSchoolOH@users.noreply.github.com>
2025-09-08 13:47:11 -0400

    Make CUGI menu look nicer.

--- src/cugiMenu.js
+++ src/cugiMenu.js
@@ -2,13 +2,15 @@
     const holder = document.getElementById("CUGI-holder");
 
     holder.appendChild(CUGI.createList([
+        { type: "boolean", key: "fitToScreen", text: "Fit to Screen", target: HazyMaze, onchange: HazyMaze.adjustScreenSize},
+        { type: "vec2", key: "overrideSize", text: "Screen Size", target: HazyMaze, onchange: HazyMaze.adjustScreenSize },
+        { type: "boolean", key: "stretchToFit", text: "Stretch to Fit", target: HazyMaze, onchange: HazyMaze.adjustScreenSize},
+        "---",
+        "---",
         { type: "vec2", key: "mazeDimensions", text: "New Size", target: HazyMaze },
         { type: "boolean", key: "isNight", text: "Night Mode", target: HazyMaze},
         { type: "button", text: "New Maze", onclick: () => {
             HazyMaze.generate();
         }},
-        { type: "boolean", key: "fitToScreen", text: "Fit to Screen", target: HazyMaze, onchange: HazyMaze.adjustScreenSize},
-        { type: "vec2", key: "overrideSize", text: "Screen Size", target: HazyMaze, onchange: HazyMaze.adjustScreenSize },
-        { type: "boolean", key: "stretchToFit", text: "Stretch to Fit", target: HazyMaze, onchange: HazyMaze.adjustScreenSize},
     ]));
 })();
